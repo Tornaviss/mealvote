cb475ce8ea4dc2d65ea553d752b41f6b893c15ec
sergey <Tornaviss@gmail.com>
2019-12-01 12:51:23 +0200

    optimize SQL generated by JPA provider by using getByEmail method and remove unnecessary @Modifying annotation under non-modifying method
      - defined entity graph for getByEmail method to fetch roles along with user so that Spring Security could make one query instead of two

--- src/main/java/com/mealvote/repository/user/CrudUserRepository.java
+++ src/main/java/com/mealvote/repository/user/CrudUserRepository.java
@@ -1,6 +1,7 @@
 package com.mealvote.repository.user;
 
 import com.mealvote.model.user.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,10 +16,10 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
     @Query("DELETE FROM User u " +
             "WHERE u.id=:id")
     int delete(@Param("id") int id);
-    
+
+    @EntityGraph(attributePaths = {"roles"}, type = EntityGraph.EntityGraphType.LOAD)
     User getByEmail(String email);
 
-    @Modifying
     @Query("SELECT DISTINCT u FROM User u " +
             "LEFT JOIN FETCH u.roles " +
             "ORDER BY u.name, u.email")
